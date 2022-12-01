1c900988a0af29fd0c464198c47eb9b40e2736ef
notkatsa <pnkatsa@gmail.com>
2022-12-01 13:53:47 +0200

    added vector all, update(), hit(), heal(), made move() for ww

--- Ergasia-oop/Source1.cpp
+++ Ergasia-oop/Source1.cpp
@@ -37,11 +37,15 @@ void Game::CreateObjects() {
 	for (int i = 0; i < num_of_trees_and_lakes; i++) {
 		Terrarain.push_back(new Entity("tree", sizeX, sizeY));
 		Terrarain.push_back(new Entity("limnh", sizeX, sizeY));
+		All.push_back(new Entity("tree", sizeX, sizeY));
+		All.push_back(new Entity("limnh", sizeX, sizeY));
 	}
 	int num_of_npc_per_team = lround(sizeX * sizeY / 15);
 	for (int i = 0; i < num_of_npc_per_team; i++) {
 		Warewolf.push_back(new npc("ww", sizeX, sizeY));
 		Vampire.push_back(new npc("vamp", sizeX, sizeY));
+		All.push_back(new npc("ww", sizeX, sizeY));
+		All.push_back(new npc("vamp", sizeX, sizeY));
 	}
 
 }
@@ -69,3 +73,185 @@ npc::npc(const char* str, int x_size, int y_size) {
 	potions = rand() % 3; // 0 1 2
 	hp = 3; // paradoxh so it withstands at least 1 attack.
 };
+
+void npc::move(Game game) {
+	if (this->name == "ww") {
+		// check for attacks
+		for (auto i = game.Vampire.begin(); i != game.Vampire.end(); ++i) {
+			if (this->get_x() != (**i).get_x()) {
+				if (this->get_y() != (**i).get_y()) //diff row and col
+					continue;
+				else if (abs(this->get_x() - (**i).get_x()) == 1) { //same row - neighboring col
+					if (this->hit(**i)) return; //in 1 tick it only does 1 action
+				}
+			}
+			else if (abs(this->get_y() - (**i).get_y()) == 1) { //same col - neighboring row 
+				if (this->hit(**i)) return;
+			}
+		}
+		// copy paste ^ same logic only for friendlys so it heals
+		// check for heals
+		for (auto i = game.Warewolf.begin(); i != game.Warewolf.end(); ++i) {
+			if (this->get_x() != (**i).get_x()) {
+				if (this->get_y() != (**i).get_y()) //diff row and col
+					continue;
+				else if (abs(this->get_x() - (**i).get_x()) == 1) { //same row - neighboring col
+					if (this->heal(**i)) return; //in 1 tick it only does 1 action
+				}
+			}
+			else if (abs(this->get_y() - (**i).get_y()) == 1) { //same col - neighboring row 
+				if (this->heal(**i)) return;
+			}
+		}
+		// move
+		do {
+			int position = rand() % 5; // 0-still 1-top 2-right 3-bottom 4-left
+			bool occupied = false;
+			if (position == 0)
+				return;
+			else if (position == 1) {
+				for (auto i = game.All.begin(); i != game.All.end(); ++i) {
+					if ((this->get_x() == (**i).get_x() && this->get_y() - (**i).get_y() == 1) || this->get_y() == 0) {
+						occupied = true;
+						break;
+					}
+				}
+				if (!occupied) {
+					(this->y)--;
+					return;
+				}
+				continue;
+			}
+			else if (position == 2) {
+				for (auto i = game.All.begin(); i != game.All.end(); ++i) {
+					if ((this->get_y() == (**i).get_y() && (**i).get_x() - this->get_x() == 1) || this->get_x() == game.sizeX) {
+						occupied = true;
+						break;
+					}
+				}
+				if (!occupied) {
+					(this->x)++;
+					return;
+				}
+				continue;
+			}
+			else if (position == 3) {
+				for (auto i = game.All.begin(); i != game.All.end(); ++i) {
+					if ((this->get_x() == (**i).get_x() && (**i).get_y() - this->get_y() == 1) || this->get_y() == game.sizeY) {
+						occupied = true;
+						break;
+					}
+				}
+				if (!occupied) {
+					(this->y)++;
+					return;
+				}
+				continue;
+			}
+			else if (position == 4) {
+				for (auto i = game.All.begin(); i != game.All.end(); ++i) {
+					if ((this->get_y() == (**i).get_y() && this->get_x() - (**i).get_x() == 1) || this->get_x() == 0) {
+						occupied = true;
+						break;
+					}
+				}
+				if (!occupied) {
+					(this->x)--;
+					return;
+				}
+				continue;
+			}
+
+		} while (true);
+	}
+	if (this->name == "vamp") {
+		// check for attacks
+		for (auto i = game.Warewolf.begin(); i != game.Warewolf.end(); ++i) {
+			if (this->get_x() != (**i).get_x()) {
+				if (this->get_y() != (**i).get_y()) //diff row and col
+					continue;
+				else if (abs(this->get_x() - (**i).get_x()) == 1) { //same row - neighboring col
+					if (this->hit(**i)) return; //in 1 tick it only does 1 action
+				}
+			}
+			else if (abs(this->get_y() - (**i).get_y()) == 1) { //same col - neighboring row 
+				if (this->hit(**i)) return;
+			}
+		}
+		// copy paste ^ same logic only for friendlys so it heals
+		// check for heals
+		for (auto i = game.Vampire.begin(); i != game.Vampire.end(); ++i) {
+			if (this->get_x() != (**i).get_x()) {
+				if (this->get_y() != (**i).get_y()) //diff row and col
+					continue;
+				else if (abs(this->get_x() - (**i).get_x()) == 1) { //same row - neighboring col
+					if (this->heal(**i)) return; //in 1 tick it only does 1 action
+				}
+			}
+			else if (abs(this->get_y() - (**i).get_y()) == 1) { //same col - neighboring row 
+				if (this->heal(**i)) return;
+			}
+		}
+		// move
+		do {
+			int position = rand() % 9; // 0-still 1-top 2-right 3-bottom 4-left
+									   // TODO 5-1:30 6-4:30 7-7:30 8-10:30
+			bool occupied = false;
+			if (position == 0)
+				return;
+			else if (position == 1) {
+				for (auto i = game.All.begin(); i != game.All.end(); ++i) {
+					if ((this->get_x() == (**i).get_x() && this->get_y() - (**i).get_y() == 1) || this->get_y() == 0) {
+						occupied = true;
+						break;
+					}
+				}
+				if (!occupied) {
+					(this->y)--;
+					return;
+				}
+				continue;
+			}
+			else if (position == 2) {
+				for (auto i = game.All.begin(); i != game.All.end(); ++i) {
+					if ((this->get_y() == (**i).get_y() && (**i).get_x() - this->get_x() == 1) || this->get_x() == game.sizeX) {
+						occupied = true;
+						break;
+					}
+				}
+				if (!occupied) {
+					(this->x)++;
+					return;
+				}
+				continue;
+			}
+			else if (position == 3) {
+				for (auto i = game.All.begin(); i != game.All.end(); ++i) {
+					if ((this->get_x() == (**i).get_x() && (**i).get_y() - this->get_y() == 1) || this->get_y() == game.sizeY) {
+						occupied = true;
+						break;
+					}
+				}
+				if (!occupied) {
+					(this->y)++;
+					return;
+				}
+				continue;
+			}
+			else if (position == 4) {
+				for (auto i = game.All.begin(); i != game.All.end(); ++i) {
+					if ((this->get_y() == (**i).get_y() && this->get_x() - (**i).get_x() == 1) || this->get_x() == 0) {
+						occupied = true;
+						break;
+					}
+				}
+				if (!occupied) {
+					(this->x)--;
+					return;
+				}
+				continue;
+			}
+
+		} while (true);
+	}
+}
